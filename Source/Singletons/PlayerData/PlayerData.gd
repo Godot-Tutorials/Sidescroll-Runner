2ef83af2fa91e67e73ae1d16b7502a6c649f30ca
Feniks <writetofeniks@gmail.com>
2020-06-29 14:35:23 +0100

    coins scene

--- Source/Singletons/PlayerData/PlayerData.gd
+++ Source/Singletons/PlayerData/PlayerData.gd
@@ -3,11 +3,18 @@ extends Node
 # Stores all player data to be transfered between a scenes and will eventually 
 # be used to create a save for the game from 
 
+var coins_collected_this_level: int = 0
+
 func _on_Collectable_collected(obj:Area2D) -> void:
 	# Check in what group objects are and decides what to do with this information
-	if obj.is_in_group("group1"): # change that later to different groups that I used
-		pass
+	if obj.is_in_group("coins"): # change that later to different groups that I used
+		_coin_collected()
 	elif obj.is_in_group("group2"): # change that later to different groups that I used
 		pass
-	else:
+	
+	if obj.get_groups() == []:
 		print(obj.name, " is not in any group")
+
+func _coin_collected():
+	coins_collected_this_level +=1
+	print(coins_collected_this_level)
